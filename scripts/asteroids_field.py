3742f9034b61c712800d29d04efd87f7172b464b
João Victor Araujo <jvsa@ic.ufal.br>
2022-01-04 21:55:47 -0300

    Still having issues on instatiating objects

--- scripts/asteroids_field.py
+++ scripts/asteroids_field.py
@@ -3,12 +3,12 @@ from scripts.asteroids_system import Asteroids
 
 def Start(cont):
     own = cont.owner
-    scene = logic.getCurrentScene()
-    scene_ina_objs = scene.objects
-    scene.addObject("Asteroide", "Asteroids_Field")
     asteroid = Asteroids(own, 3)
     asteroid.positions = [0.8,0.6]
 
 def Update(cont):
     asteroid = cont.owner
     asteroid.AsteroidsMove()
+    cene = logic.getCurrentScene()
+    cene_ina_objs = cene.objects
+    cene.addObject("Asteroide", "Asteroids_Field")
