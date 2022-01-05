a2be45c44bbaac715cd9f5cb80969db65c4dcf29
João Victor Araujo <jvsa@ic.ufal.br>
2022-01-04 23:23:07 -0300

    Instantiantion done

--- scripts/asteroids_field.py
+++ scripts/asteroids_field.py
@@ -4,11 +4,12 @@ from scripts.asteroids_system import Asteroids
 def Start(cont):
     own = cont.owner
     asteroid = Asteroids(own, 3)
+    
+    cene = logic.getCurrentScene()
+    cene_ina_objs = cene.objects
+    cene.addObject("Asteroide", "Asteroids_Field")
     asteroid.positions = [0.8,0.6]
 
 def Update(cont):
     asteroid = cont.owner
     asteroid.AsteroidsMove()
-    cene = logic.getCurrentScene()
-    cene_ina_objs = cene.objects
-    cene.addObject("Asteroide", "Asteroids_Field")
