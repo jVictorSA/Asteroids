1d1dcc582f9654440c45764816d9eca9316a8960
Rafael <rafael2mcont@gmail.com>
2022-01-10 15:17:54 -0300

    Best explanation in prints

--- scripts/asteroid_destroy.py
+++ scripts/asteroid_destroy.py
@@ -6,9 +6,10 @@ def Update(cont):
     scene = logic.getCurrentScene()
     
     col = cont.sensors["Collision"].positive
-    print("MAIN, col=", col)
+    print("Level 1, col=", col, "name=", asteroid.name)
     if col==True:
-        print("POSITIVE")
+        #print("POSITIVE")
+        print("Level 2")
         scene.addObject("asteroid_mini", asteroid)
         scene.addObject("asteroid_mini", asteroid)
         scene.addObject("asteroid_mini", asteroid)
